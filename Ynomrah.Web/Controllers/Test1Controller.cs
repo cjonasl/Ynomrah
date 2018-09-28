c6c021e5b2d274791f19b779e41a1d6baaa472b3
cjonasl <jonas5669@outlook.com>
2018-09-28 22:51:40 +0100

    Commit 2018-09-28 kl. 22:51

---
+++ Ynomrah.Web/Controllers/Test1Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ynomrah.Web.Controllers
+{
+    public class AAA
+    {
+        public static string str1 = "<h1>Hello</h1>";
+
+        public override string ToString()
+        {
+            return "<p>Jag heter Jonas</p>";
+        }
+    }
+
+    public class BBB
+    {
+        public void Write()
+        {
+            AAA aaa = new AAA();
+            HttpContext.Current.Response.Write(aaa);
+        }
+    }
+
+
+    public class Test1Controller : Controller
+    {
+        // GET: Test1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ynomrah.Web/Views/Test1/Index.aspx
@@ -0,0 +1,14 @@
+﻿ <%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+ <%=Ynomrah.Web.Controllers.AAA.str1%>
+ <%:Ynomrah.Web.Controllers.AAA.str1%>
+ <% Ynomrah.Web.Controllers.BBB bbb = new Ynomrah.Web.Controllers.BBB(); bbb.Write(); %>
+</body>
+</html>
